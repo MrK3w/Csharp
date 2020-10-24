19dd8c43c6ca65a1aa77089619a4f0d15bbba060
Jakub Pietrus <pie0056@vsb.cz>
2020-10-24 11:57:22 +0200

    MiksaWasHere

---
+++ lesson5/tetirs/tetirs/BlockFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lesson5
+{
+    public static class BlockFactory
+    {
+        public static Tetromino Build(int i)
+        {
+            Random brick = new Random();
+            int brickos = brick.Next(1, 8);
+            switch (brickos)
+            {
+                case 1:
+                    return new Line(new Brick[4], 0, i * 3 + 1, 0, ConsoleColor.Cyan);
+                case 2:
+                    return new Lbrick(new Brick[4], 0, i * 3 + 1, 0, ConsoleColor.White);
+                case 3:
+                    return new Square(new Brick[4], 0, i * 3 + 1, 0, ConsoleColor.Yellow);
+                case 4:
+                    return new Jbrick(new Brick[4], 0, i * 3 + 1, 0, ConsoleColor.DarkBlue);
+                case 5:
+                    return new Sblock(new Brick[4], 0, i * 3 + 1, 0, ConsoleColor.DarkGreen);
+                case 6:
+                    return new Tblock(new Brick[4], 0, i * 3 + 1, 0, ConsoleColor.Magenta);
+                case 7:
+                    return new Zblock(new Brick[4], 0, i * 3 + 1, 0, ConsoleColor.Red);
+                default:
+                    return new Tetromino();
+            }
+        }
+    }
+}
--- lesson5/tetirs/tetirs/Program.cs
+++ lesson5/tetirs/tetirs/Program.cs
@@ -9,46 +9,18 @@ namespace lesson5
         public int X;
         public int Y;
     }
-
-    class Progam
+    class Program
     {
+        const int CAPACITY = 10;
         static void Main(string[] args)
         {
-            Random brick = new Random();
-            const int capacity = 10;
             Console.BackgroundColor = ConsoleColor.Black;
-            Queue fronta = new Queue(capacity);
-            Tetromino[] Tetromina = new Tetromino[capacity];
-            int poz = 0;
-            for (int i = 0; i < capacity; i++)
+            Queue fronta = new Queue(CAPACITY);
+            Tetromino[] tetromina = new Tetromino[CAPACITY];
+            for (int i = 0; i < CAPACITY; i++)
             {
-               int brickos = brick.Next(1, 8);
-                switch(brickos)
-                {
-                    case 1: Tetromina[i] = new Line(new Brick[4], 0, i*3+1+poz, 1, ConsoleColor.Cyan);
-                        poz += 2;
-                        break;
-                    case 2:
-                        Tetromina[i] = new Lbrick(new Brick[4], 0, i * 3 + 1+poz, 0, ConsoleColor.White);
-                        break;
-                    case 3:
-                        Tetromina[i] = new Square(new Brick[4], 0, i * 3 + 1+ poz, 0, ConsoleColor.Yellow);
-                        break;
-                    case 4:
-                        Tetromina[i] = new Jbrick(new Brick[4], 0, i * 3 + 1+ poz, 0, ConsoleColor.DarkBlue);
-                        break;
-                    case 5:
-                        Tetromina[i] = new Sblock(new Brick[4], 0, i * 3 + 1+ poz, 0, ConsoleColor.DarkGreen);
-                        break;
-                    case 6:
-                        Tetromina[i] = new Tblock(new Brick[4], 0, i * 3 + 1+ poz, 0, ConsoleColor.Magenta);
-                        break;
-                    case 7:
-                        Tetromina[i] = new Zblock(new Brick[4], 0, i * 3 + 1+ poz, 0, ConsoleColor.Red);
-                        break;
-                }
-
-                fronta.Enqueue(Tetromina[i]);
+                tetromina[i] = BlockFactory.Build(i);
+                fronta.Enqueue(tetromina[i]);
             }
             fronta.Print();
             Console.ForegroundColor = ConsoleColor.White;
